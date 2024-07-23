9be29d515768108b11cffbcc18a6440dac7c80a9
dawid mrotek <DawidMrotek@protonmail.ch>
2024-07-23 03:49:33 +0200

    [ADD] tracks predictions

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from utils import read_video, save_video
+from trackers import Tracker
 
 def main():
     # read video
     video_frames = read_video("input_videos/08fd33_4.mp4")
+
+    # initalize tracker
+    tracker = Tracker('models/best.pt')
+
+    tracks = tracker.get_object_tracks(video_frames, read_from_stubs=True, stub_path='stubs/track_stubs.pkl')
 
     # save video
     save_video(video_frames, "output_videos/output_video.avi")
