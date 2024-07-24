eda11274611bb5fa1be4f56409167777fa856312
dawid mrotek <DawidMrotek@protonmail.ch>
2024-07-24 05:37:44 +0200

    [ADD] assign players to teams

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from utils import read_video, save_video
 from trackers import Tracker
 import cv2
+from team_assigner import TeamAssigner
+
 
 def main():
     # read video
@@ -10,6 +12,16 @@ def main():
     tracker = Tracker('models/best.pt')
 
     tracks = tracker.get_object_tracks(video_frames, read_from_stubs=True, stub_path='stubs/track_stubs.pkl')
+
+    # assign players to teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], tracks['players'][0])
+
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num], track['bbox'], player_id)
+            tracks['players'][frame_num][player_id]['team'] = team
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
 
     # vvv draw output vvv
