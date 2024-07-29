400da4160edf9ff16ebfabc533d12100e9551b85
dawid mrotek <DawidMrotek@protonmail.ch>
2024-07-29 15:28:31 +0200

    [ADD] perspective transformer

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
+from view_transformer import ViewTransformer
 
 
 def main():
@@ -24,6 +25,9 @@ def main():
                                                                               read_from_stub=True,
                                                                               stub_path='stubs/camera_movement_stub.pkl')
     camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
+
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
 
     # interpolate ball positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
